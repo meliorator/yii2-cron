99a1fea26677290267cd7765d75a2f91b621abfb
Mykola Laba <me@laba.net.ua>
2023-01-24 19:33:02 +0200

    finetune locate log file

--- commands/JobController.php
+++ commands/JobController.php
@@ -4,6 +4,7 @@
 namespace meliorator\cron\commands;
 
 use meliorator\cron\models\CronJob;
+use meliorator\cron\Module;
 use yii\console\Controller;
 use yii\helpers\Console;
 
@@ -21,17 +22,24 @@ class JobController extends Controller
         $job = CronJob::findOne($id);
         $run = $job->run();
 
+        $module = Module::getInstance();
+
+        $logFile = strtr('{yii}/{file}', [
+            '{yii}' => $module->yiiFile,
+            '{file}' => $job->logfile
+        ]);
+
         Console::output('Process is finished, exit code: #' . $run->exit_code);
         Console::output($run->output);
-        Console::output($job->logfile);
+        Console::output($logFile);
 
         if($job->logfile!="") {
-            error_log($run->output, 3, $job->logfile);
+            error_log($run->output, 3, $logFile);
         }
 
         if (!empty($run->error_output)) {
             if($job->logfile!="") {
-                error_log($run->error_output, 3, $job->logfile);
+                error_log($run->error_output, 3, $logFile);
             }
             Console::output($run->error_output);
         }
